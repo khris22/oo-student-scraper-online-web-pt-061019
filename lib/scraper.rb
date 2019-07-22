352cb1649a8ac9450148b1b29d41ced1416a1fb0
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-22 11:33:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,23 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css(".roster-cards-container").text
+    binding.pry
   end
 
+
+
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
+#
+# Scraper.new.scrape_index_page
